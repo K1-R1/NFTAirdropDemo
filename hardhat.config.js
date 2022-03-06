cdd58d50da4ba5f06e3e22cae67b1bb876d31127
K1-R1 <kyranrawlinson@gmail.com>
2022-03-06 14:36:32 +0000

    Updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-gas-reporter');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +19,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      { version: '0.8.4', settings },
+      { version: '0.7.6', settings },
+      { version: '0.6.12', settings },
+      { version: '0.5.16', settings },
+    ],
+  },
 };
